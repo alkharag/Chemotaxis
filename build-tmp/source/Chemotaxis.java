3001ef4c9590daea232144de75b110a29c8c0b37
alkharag <kharag.alex_sa@yahoo.com>
2014-09-22 11:09:05 -0700

    hope it works

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,9 +1,25 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 boolean followMouse = false;
 boolean awayMouse = false;
 Bacteria [] colony;
 
  //declare bacteria variables here   
- void setup()   
+ public void setup()   
  {     
  	size(500, 500);
  	colony = new Bacteria[50];
@@ -13,7 +29,7 @@ Bacteria [] colony;
  	}
 
  }   
- void draw()   
+ public void draw()   
  {   
  	background(0,50,130);
  	for (int i =0; i < colony.length; i ++) {
@@ -44,7 +60,7 @@ Bacteria [] colony;
 
  	}
 
- 	void show()
+ 	public void show()
  	{
  		fill(0);
  		ellipse(bacX-2, bacY+2, 5, 5);
@@ -55,7 +71,7 @@ Bacteria [] colony;
  		ellipse(bacX+2, bacY-2, 5, 5);
  	}
  	
- 	void move()
+ 	public void move()
  	{
  		
  		bacX = bacX + (int)(Math.random()*6-3);
@@ -65,7 +81,7 @@ Bacteria [] colony;
 
 
  	}
- 	void follow()
+ 	public void follow()
  	{
  		if (followMouse == true)
  		{
@@ -85,3 +101,12 @@ Bacteria [] colony;
 
  }    
  
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
